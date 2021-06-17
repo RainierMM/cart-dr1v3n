a0f935a6788d29833b029a3effb9af03b51aa80e
Rainier <rainiermichelena@gmail.com>
2021-06-17 13:43:34 -0500

    End design

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,8 +14,19 @@ export const Card = () => {
   return (
     <div className={styles.card}>
       <div className={styles.gridContainer}>
-        <p className={styles.buyCart}>Carrito de compras</p>
+        <div className={styles.buyCart}>Carrito de compras</div>
         <Product />
+        <div className={styles.divider}></div>
+
+        <span className={styles.subtotalText}>Subtotal:</span>
+        <div className={styles.subtotalNumber}>S/.1000</div>
+        <span className={styles.shippingText}>Envío</span>
+        <span className={styles.shippingNumber}>S/.400</span>
+        <div className={styles.divider}></div>
+        <span className={styles.totalPriceText}>Total:</span>
+        <span className={styles.totalPriceNumber}>S/.1100</span>
+        <button className={styles.payButton}>Pagar</button>
+        <span className={styles.clearCart}>Limpiar Carrito</span>
       </div>
     </div>
   );
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,62 @@
-import React, { useContext } from "react";
+import React, { useContext, Fragment, useState } from "react";
 import styles from "./styles.module.css";
-
+import DeleteIcon from "@material-ui/icons/Delete";
 import { CartAppContext } from "../context";
 
 const Product = () => {
   const { data } = useContext(CartAppContext);
 
-  return <div className={styles.divider}></div>;
+  console.log(data);
+
+  return (
+    <>
+      {data && data.length > 0
+        ? data.map(({ id, name, price, image, amount }) => (
+            <Fragment key={id}>
+              <div className={styles.divider}></div>
+
+              <img className={styles.image} src={image} />
+
+              <div className={styles.description}>
+                <p className={styles.productName}>{name}</p>
+                <div className={styles.productAmountBox}>
+                  <p className={styles.productAmount}>Cantidad</p>
+                  <div className={styles.radioButtonsArea}>
+                    <div className={styles.productAmountButton}>
+                      <div className={styles.sustract}>-</div>
+                    </div>
+                    <span className={styles.productAmountNumber}>{amount}</span>
+                    <div className={styles.productAmountButton}>
+                      <div className={styles.add}>+</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div styles={styles.nullBox}></div>
+              <div styles={styles.priceBox}>
+                <p className={styles.productPrice}>{`S/.${price}`}</p>
+                <div className={styles.deleteProduct}>
+                  <DeleteIcon style={{ color: "#FFFFFF", margin: "-5px" }} />
+                  <span
+                    style={{
+                      color: "#FFFFFF",
+                      fontFamily: "Roboto",
+                      fontStyle: "normal",
+                      fontWeight: "normal",
+                      fontSize: "0.5em",
+                      lineHeight: "0.563em",
+                      marginLeft: "0.234em",
+                    }}
+                  >
+                    Quitar producto
+                  </span>
+                </div>
+              </div>
+            </Fragment>
+          ))
+        : null}
+    </>
+  );
 };
 
 export default Product;
